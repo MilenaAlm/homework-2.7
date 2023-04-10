b68a1309b318e72c8d43eb9ac97642908929b9ff
milenaalmazyan <ams007@mail.ru>
2023-04-11 00:29:46 +0300

    Первый коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class Main {
+    private static Employee[] employees = new Employee[10];
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        employees[0] = new Employee("Ivan Ivanov Ivanovich1", 1, 120_000);
+        employees[1] = new Employee("Ivan Ivanov Ivanovich2", 2, 110_000);
+        employees[2] = new Employee("Ivan Ivanov Ivanovich3", 3, 89_000);
+        employees[3] = new Employee("Ivan Ivanov Ivanovich4", 4, 70_000);
+        employees[4] = new Employee("Ivan Ivanov Ivanovich5", 5, 68_000);
+        employees[5] = new Employee("Ivan Ivanov Ivanovich6", 1, 126_000);
+        employees[6] = new Employee("Ivan Ivanov Ivanovich7", 2, 105_000);
+        employees[7] = new Employee("Ivan Ivanov Ivanovich8", 3, 89_000);
+        employees[8] = new Employee("Ivan Ivanov Ivanovich9", 4, 78_000);
+        employees[9] = new Employee("Ivan Ivanov Ivanovich10", 5, 90_000);
+
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
